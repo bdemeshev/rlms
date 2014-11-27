5709a2e586f4799fe73f9146dd1d3969b6efb9a4
Boris Demeshev <boris.demeshev@gmail.crom>
2014-11-27 20:31:37 +0300

    nine2na

--- R/rlms.R
+++ R/rlms.R
@@ -13,10 +13,12 @@ NULL
 #' Read rlms data and all the meta information. Destroy useless attributes.
 #'
 #' @param file the filename
+#' @param suppress logical, if true the default message is suppressed
+#' @param nine2na automatically convert 99999999 to NA for numeric variables
 #' @return dataframe
 #' @examples
 #' read.rlms("r21i_os24a.sav")
-read.rlms <- function(file, suppress=FALSE) {
+read.rlms <- function(file, suppress=FALSE, nine2na=TRUE) {
   df <- read.spss(file, to.data.frame = TRUE, reencode = "UTF-8")
   attr(df, "codepage") <- NULL
   
@@ -41,9 +43,16 @@ read.rlms <- function(file, suppress=FALSE) {
     }
   }
   
+  # replace 99999996 for numeric variables
+  if (nine2na) {
+    for (i in 1:ncol(df)) {  
+      if (class(df[,i])=="numeric") df[,i] <- ifelse(df[,i]>99999995,NA,df[,i])  
+    }
+  }
+  
   
   attr(df,"var_meta") <- var_meta
-  attr(df,"value_meta") <- value_meta
+  attr(df,"value_meta") <- value_meta  
   
   if (!suppress) {
     message("Variable labels: attr(df, 'var_meta'). Value labels: attr(df, 'value_meta').")

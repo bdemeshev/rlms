d3565c46df296fa08b7abb9fe2356139f4506579
Boris Demeshev <boris.demeshev@gmail.com>
2016-01-06 01:30:41 +0300

    doc update

--- inst/tests/rlms_tests.R
+++ inst/tests/rlms_tests.R
@@ -3,4 +3,3 @@ test_that("extract wave/level/sample from filename", {
   expect_equal(as.character(rlms_fileinfo("r06hall23.sav")$level), "household")
   expect_equal(as.character(rlms_fileinfo("r06hall23.sav")$sample), "all")
 })
-
